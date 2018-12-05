39b7b40a688beefb46b644a6099254988417245f
Yolo <tema-sid@mail.ru>
2018-12-05 03:54:03 +0300

    SaveForm

---
+++ BitTorrent/SaveForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitTorrent
+{
+    public partial class SaveForm : Form
+    {
+        public SaveForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+           FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = dialog.SelectedPath;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
